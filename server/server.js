199ce2f1888573986fe3dd73f438292eeafb6b53
Iván Canga <ivancanga@outlook.com>
2020-03-15 02:20:45 -0300

    express-session, auth + admin added.

--- server/server.js
+++ server/server.js
@@ -1,110 +1,107 @@
 const express = require("express");
+const server = express();
+const session = require("express-session");
+
 const bodyParser = require("body-parser");
+
 const jwt = require("jsonwebtoken");
 const signature = "39486747";
+
 const cors = require("cors");
-const mysql = require("mysql");
-const server = express();
+
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize("mysql://root@localhost:3306/delilah-resto");
-
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "delilah-resto"
-});
-
-connection.connect(function(error) {
-  if (error) {
-    throw error;
-  } else {
-    console.log("Conexion correcta.");
-  }
-});
 
 server.listen(3000, () => {
   console.log("Servidor iniciado");
 });
 
-server.use(bodyParser.json(), cors());
+server.use(
+  session({
+    secret: "delilah-restoAUTH",
+    resave: true,
+    saveUninitialized: true
+  }),
+  bodyParser.json(),
+  cors()
+);
+
+// server.post("/register", (req, res) => {
+//   const { username, name, email, phone, address, password, admin } = req.body;
+//   let insert = `INSERT INTO users(username,name,email,phone,address,password,admin)
+//     VALUES ('${username}','${name}','${email}',${phone},'${address}','${password}',${admin})`;
+//   connection.query(insert);
+//   res.json("Usuario agregado a la base de datos");
+// });
+
+// server.post("/products", (req, res) => {
+//   const { name, photo, description, price } = req.body;
+//   let insert = `INSERT INTO menu(name,photo,description,price)
+//     VALUES ('${name}','${photo}','${description}',${price})`;
+//   connection.query(insert);
+//   res.json("Plato agregado a la base de datos");
+// });
+
+// server.get("/products", (req, res) => {
+//   connection.query("SELECT * FROM menu", (error, results, fields) => {
+//     if (error) throw error;
+//     res.json(results);
+//   });
+// });
+
+// Muestra todos los items del menu
+// connection.query("SELECT * FROM products", function(error, results, fields) {
+//   if (error) throw error;
+//   results.forEach(result => {
+//     console.log(result);
+//   });
+// });
+
+function auth(req, res, next) {
+  if (req.session && req.session.admin) {
+    return next();
+  } else {
+    return res
+      .status(401)
+      .json(
+        "Se necesitan permisos de administrador para acceder a esta ruta. Por favor, logea un usuario administrador."
+      );
+  }
+}
 
 server.post("/login", userLogin, (req, res) => {
   const { userData } = req;
-  console.log(userData);
   // const isAdmin = userData[0].admin;
   // const token = getToken({ userData });
   // const activeUser = { token, isAdmin };
   res.status(200).json(userData);
 });
 
-server.post("/register", (req, res) => {
-  const { username, name, email, phone, address, password, admin } = req.body;
-  let insert = `INSERT INTO users(username,name,email,phone,address,password,admin)
-    VALUES ('${username}','${name}','${email}',${phone},'${address}','${password}',${admin})`;
-  connection.query(insert);
-  res.json("Usuario agregado a la base de datos");
-});
-
-server.post("/products", (req, res) => {
-  const { name, photo, description, price } = req.body;
-  let insert = `INSERT INTO menu(name,photo,description,price)
-    VALUES ('${name}','${photo}','${description}',${price})`;
-  connection.query(insert);
-  res.json("Plato agregado a la base de datos");
-});
-
-server.get("/products", (req, res) => {
-  connection.query("SELECT * FROM menu", (error, results, fields) => {
-    if (error) throw error;
-    res.json(results);
-  });
-});
-
-// Muestra todos los items del menu
-connection.query("SELECT * FROM products", function(error, results, fields) {
-  if (error) throw error;
-  results.forEach(result => {
-    console.log(result);
-  });
-});
-
-// function userLogin(req, res, next) {
-//   const { user, password } = req.body;
-//   connection.query(
-//     `SELECT id,name,email,phone,address,admin FROM users
-//      WHERE username = '${user}' AND password = '${password}' `,
-//     (error, userData, fields) => {
-//       if (error) throw error;
-//       if (userData.length !== 0) {
-//         req.userData = userData;
-//         next();
-//       } else {
-//         res.status(400).json("Credenciales incorrectas.");
-//       }
-//     }
-//   );
-// }
-
 function userLogin(req, res, next) {
-  const { user, password } = req.body;
-  sequelize
-    .query(
-      "SELECT id,name,email,phone,address,admin FROM users WHERE username = ? AND password = ?",
-      {
-        replacements: [user, password],
-        type: sequelize.QueryTypes.SELECT
-      }
-    )
-    .then(results => {
-      console.log(results);
-      if (results.length !== 0) {
-        req.userData = results[0];
-        next();
-      } else {
-        res.status(400).json("Credenciales incorrectas.");
-      }
-    });
+  if(!req.session.user) {
+    const { user, password } = req.body;
+    sequelize
+      .query(
+        "SELECT id,name,email,phone,address,admin FROM users WHERE username = ? AND password = ?",
+        {
+          replacements: [user, password], 
+          type: sequelize.QueryTypes.SELECT
+        }
+      )
+      .then(result => {
+        if (result.length != 0) {
+          if (result[0].admin) req.session.admin = true;
+          req.session.user = user;
+          req.userData = result[0];
+          console.log(req.session);
+          next();
+        } else {
+          res.status(400).json("Credenciales incorrectas.");
+        }
+      });
+  }else{
+    res.status(409).json('Ya te encuentras logeado. Por favor deslogea o reinicia el servidor.');
+  }
 }
 
 function getToken(data) {
@@ -112,11 +109,18 @@ function getToken(data) {
   return resp;
 }
 
+// PRODUCTOS
+
+// server.put("/products", isLogged, modifyProduct, (req, res) => {
+//   res.status(200).json("producto modificado con exito");
+// })
+
+// function modifyProduct
+
 // ORDENES
 
-server.get("/orders", getOrders, getProducts, (req, res) => {
+server.get("/orders", auth, getOrders, getProducts, (req, res) => {
   const { orderData } = req;
-  console.log("Paso al request, ", orderData);
   res.status(200).json(orderData);
 });
 
@@ -129,7 +133,6 @@ function getOrders(req, res, next) {
       }
     )
     .then(results => {
-      console.log(results);
       if (results.length !== 0) {
         req.orderData = results;
         next();
@@ -151,15 +154,17 @@ function getProducts(req, res, next) {
         .then(results => {
           order.productos = results;
           order.total = order.productos
-            .map(producto => producto.price)
+            .map(producto => producto.price * producto.qty)
             .reduce((acc, cur) => acc + cur);
         })
     );
   });
-  Promise.all(promises).then(() => {
-    req.orderData = req.orderData;
-    next();
-  }).catch(err => {
-    console.log(err);
-  });;
+  Promise.all(promises)
+    .then(() => {
+      req.orderData = req.orderData;
+      next();
+    })
+    .catch(err => {
+      console.log(err);
+    });
 }
